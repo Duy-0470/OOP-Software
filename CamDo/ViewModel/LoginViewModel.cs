e5333d4e7300defd4c05d39b0d01f1babbec1116
Duy-0470 <20520470@gm.uit.edu.vn>
2022-11-21 21:55:54 +0700

    Minor fix

---
+++ CamDo/ViewModel/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CamDo.ViewModel
+{
+    public class LoginViewModel
+    {
+        public ICommand ClosingWindowCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            ClosingWindowCommand = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                Application.Current.Shutdown();
+                App.Current.Shutdown();
+            });
+        }
+    }
+}
