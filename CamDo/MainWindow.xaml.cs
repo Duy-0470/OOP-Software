1fdc49aa96a8dd10efd91845c5217487bd9262b7
Duy-0470 <20520470@gm.uit.edu.vn>
2022-11-30 01:06:09 +0700

    Navigation bar add 90% ( Icon not included )

--- CamDo/MainWindow.xaml.cs
+++ CamDo/MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CamDo
@@ -23,8 +22,45 @@ namespace CamDo
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new ViewModel.MainViewModel();
+        }
 
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_customer.Visibility = Visibility.Collapsed;
+                tt_pawn.Visibility = Visibility.Collapsed;
+                tt_remind.Visibility = Visibility.Collapsed;
+                tt_search.Visibility = Visibility.Collapsed;
+                tt_manage.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_customer.Visibility = Visibility.Visible;
+                tt_pawn.Visibility = Visibility.Visible;
+                tt_remind.Visibility = Visibility.Visible;
+                tt_search.Visibility = Visibility.Visible;
+                tt_manage.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //img.bg.opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            //imh.bg.opcaity - 0.3;
+        }
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
         }
     }
 }
