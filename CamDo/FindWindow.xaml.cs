46c5d52f0d0b0beacdd2aad7dfba501fa9832536
Duy-0470 <20520470@gm.uit.edu.vn>
2022-12-02 21:46:15 +0700

    Change theme and nav for all Window

---
+++ CamDo/FindWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CamDo
+{
+    /// <summary>
+    /// Interaction logic for FindWindow.xaml
+    /// </summary>
+    public partial class FindWindow : Window
+    {
+        public FindWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_customer.Visibility = Visibility.Collapsed;
+                tt_pawn.Visibility = Visibility.Collapsed;
+                tt_remind.Visibility = Visibility.Collapsed;
+                tt_search.Visibility = Visibility.Collapsed;
+                tt_manage.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_customer.Visibility = Visibility.Visible;
+                tt_pawn.Visibility = Visibility.Visible;
+                tt_remind.Visibility = Visibility.Visible;
+                tt_search.Visibility = Visibility.Visible;
+                tt_manage.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
